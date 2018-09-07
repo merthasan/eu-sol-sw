f183347c6f5682cad61f732721687a8264bb6dff
Μert <merthasan@mert-mbp.local>
2018-09-07 19:38:09 +0300

    added solution for problem 2

---
+++ euler2sw/euler2sw/main.swift
@@ -0,0 +1,18 @@
+import Foundation
+
+var i1 = 1
+var i2 = 2
+var i = i1 + i2
+var sum = 0
+
+while (i < 4000000) {
+    if (i % 2 == 0) {
+        sum += i
+    }
+    i1 = i2
+    i2 = i
+    i = i1 + i2
+}
+sum += 2 //+2 because 2 is an even number smaller than 4 million but is not added to sum in the function above
+
+print("The sum of all even numbers smaller than 4 million is \(sum)")
