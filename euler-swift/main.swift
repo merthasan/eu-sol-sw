551fd4753ab919fb81c1d60f3bad84f11c734ebb
Μert <merthasan@mert-mbp.local>
2018-09-07 19:29:11 +0300

    created stuff

--- euler-swift/main.swift
+++ euler-swift/main.swift
@@ -1,12 +1,14 @@
-//
-//  main.swift
-//  euler-swift
-//
-//  Created by Mert Hasan on 7.09.2018.
-//  Copyright © 2018 Mert Hasan. All rights reserved.
-//
 
 import Foundation
 
-print("Hello, World!")
+var i = 1
+var sum = 0
 
+while (i < 1000) {
+    if (i % 3 == 0 || i % 5 == 0) { //checks if the number is a multiple of either 3 or 5
+        sum += i //increases sum by i
+    }
+    i += 1 //increases i by 1 to check the next number
+}
+
+print("Sum of all the multiples of 3 or 5 below 1000 is \(sum)")
