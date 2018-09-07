207ed2c7554ff8e05866966ea09ad83d9ac17296
Μert <merthasan@mert-mbp.local>
2018-09-07 19:10:33 +0300

    Initial Commit

---
+++ euler-swift/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  euler-swift
+//
+//  Created by Mert Hasan on 7.09.2018.
+//  Copyright © 2018 Mert Hasan. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
